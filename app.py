8ae1dfbc521d5a2e672867130c02a14440947c57
Jayant Sharma <jayantsharma365@gmail.com>
2024-11-11 17:37:47 +0000

    Fix Gradio error, upgrade dependencies, and disable GPU

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 from transformers import pipeline
 import gradio as gr
+import os
 
-model = pipeline("summarization")
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+os.environ["TF_FORCE_GPU_ALLOW_GROWTH"] = "false"
+
+model = pipeline("summarization", model="Falconsai/text_summarization")
 
 def predict(prompt):
     summary = model(prompt)[0]["summary_text"]
     return summary
 
-with gr.Blocks() as demo:
-    textbox = gr.Textbox(placeholder="Enter text block to summarize", lines=4)
-    gr.Interface(fn=predict, inputs=textbox, outputs="text")
+# with gr.Blocks() as demo:
+textbox = gr.Textbox(placeholder="Enter text block to summarize", lines=4)
+interface = gr.Interface(fn=predict, inputs=textbox, outputs="text")
 
-demo.launch()
+interface.launch()
